56807effe58bd9fb5b484cc49fa222be6cea4879
Nico Pies <mail@nicopies.de>
2026-08-30 22:21:47 +0200

    Mobile View

--- portfolio_v3/app/components/Projekt.vue
+++ portfolio_v3/app/components/Projekt.vue
@@ -29,11 +29,11 @@
 </script>
 
 <template>
-  <NuxtLink :to="project.path" class="transition-all hover:rotate-0 duration-200 group w-[75%] rounded-lg p-[1rem] flex flex-col justify-center items-start rotate-[var(--r)]  cursor-[url('/pointer.svg'),_pointer]" :class="index > 0 ? 'pointer-events-none' : ''" :style="{backgroundColor: bgColor ,filter: `blur(${blurAmount}px)`, transform: transformValue}" @mouseenter="isHovered = true" @mouseleave="isHovered = false">
+  <NuxtLink :to="project.path" class="transition-all hover:rotate-0 duration-200 group lg:w-[75%] rounded-lg p-[1rem] flex flex-col justify-center items-start rotate-[var(--r)]  cursor-[url('/pointer.svg'),_pointer]" :class="index > 0 ? 'pointer-events-none' : ''" :style="{backgroundColor: bgColor ,filter: `blur(${blurAmount}px)`, transform: transformValue}" @mouseenter="isHovered = true" @mouseleave="isHovered = false">
     <h3 class="font-kavoon text-[#FFFAEF] text-[2rem] mb-[0.5rem]">{{ project.title }}</h3>
-    <p class="mb-[0.5rem] font-taviraj text-[#FFFAEF]">{{ project.description }}</p>
-    <div class="flex flex-row gap-[1.5rem] mb-[1.5rem] w-[90%]">
-        <div v-for="skill in project.skills" class="flex flex-row items-center justify-center font-taviraj text-[#FFFAEF] gap-[0.5rem] bg-black/10 rounded-lg p-[0.5rem]">
+    <p class="mb-[0.5rem] font-taviraj text-[#FFFAEF] text-[0.8f5rem]">{{ project.description }}</p>
+    <div class="flex flex-row gap-[0.5rem] lg:gap-[1.5rem] mb-[1.5rem] w-[90%] flex-wrap">
+        <div v-for="skill in project.skills" class="flex flex-row items-center justify-center font-taviraj text-[#FFFAEF] text-[0.75rem] lg:text-[1rem] gap-[0.5rem] bg-black/10 rounded-lg p-[0.5rem]">
             <img :src="skill.bild" :alt="skill.name" class="max-h-[1.3rem]"/>
             <div>{{ skill.name }}</div>
         </div>
--- portfolio_v3/app/components/footer.vue
+++ portfolio_v3/app/components/footer.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div ref="navDivider" class="z-50 w-full h-[0.15rem] fixed bottom-[10rem] left-0 bg-[#F5F0E4]"></div>
+    <div ref="footDivider" class="z-50 w-full h-[0.15rem] bottom-[10rem] left-0 bg-[#F5F0E4]"></div>
     <div class="z-50 w-full h-[10rem] font-taviraj bg-[#FFFAEF] p-[2rem]">
         <div class="flex flex-row">
             <div class="flex flex-col">
